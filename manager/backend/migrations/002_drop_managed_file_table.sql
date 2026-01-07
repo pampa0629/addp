b460b007454c49e2d2e1c87ff93c7d28ea0b5101
pampa0629 <38243865+pampa0629@users.noreply.github.com>
2026-01-07 17:53:28 +0800

    v0.0.21

--- manager/backend/migrations/002_drop_managed_file_table.sql
+++
@@ -1,5 +0,0 @@
--- 删除未使用的 managed_file 表
--- 该表从未被实际使用，仅有模型定义和转换方法（未调用）
--- 删除时间: 2026-01-07
-
-DROP TABLE IF EXISTS manager.managed_file;
--- manager/backend/migrations/003_drop_managed_table.sql
+++
@@ -1,6 +0,0 @@
--- 删除 managed_table 表，统一使用 Meta 模块管理元数据
--- 原因: 与 Meta 模块功能重叠，Manager 和 Meta 存储重复的表元数据
--- 改为: Manager 通过 Meta API 获取表元数据，统一元数据管理
--- 删除时间: 2026-01-07
-
-DROP TABLE IF EXISTS manager.managed_table;
